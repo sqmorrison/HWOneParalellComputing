d72442e959f826e563cfbee0c3d7c062e4bfdf5c
Quinn Morrison <96103133+sqmorrison@users.noreply.github.com>
2026-02-15 15:46:57 -0600

    problem 1 complete

--- problem_one.cpp
+++ problem_one.cpp
@@ -2,34 +2,42 @@
 #include <iostream>
 
 int main(int argc, char *argv[]){
-    int size, rank, sent_int = 0;
+    int size, rank, n, sent_int = 0;
     MPI_Init(&argc,&argv);
     
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
-    if(rank == 0){
-        //send to next node
-        std::cout << "Sending initilized with index: " << sent_int << std::endl;
-        MPI_Send(&sent_int, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        //recieve from last node
-        MPI_Recv(&sent_int, 1, MPI_INT, (size - 1), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        std::cout << "Last Recieved Index: " << sent_int << std::endl;
-    }
-    else {
-        //recieve from left neighbor
-        MPI_Recv(&sent_int, 1, MPI_INT, (rank - 1), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        sent_int++;
-        std::cout << "Recieved int: " << sent_int << " on rank: " << rank << std::endl;
-        
-        //if last process, send to 0, otherwise end to right neighbor
-        if(rank == size - 1){
-            //send to start process
-            std::cout << "sending back to process 0: " << sent_int << std::endl;
-            MPI_Send(&sent_int, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    n = atoi(argv[1]);
+    
+    for(int i = 0; i < n; i++){
+        if(rank == 0){
+            if(i > 1){
+                sent_int++;
+            }
+            //send to next node
+            std::cout << "Sending from process 0 with int: " << sent_int << std::endl;
+            MPI_Send(&sent_int, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+            //recieve from last node
+            MPI_Recv(&sent_int, 1, MPI_INT, (size - 1), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            std::cout << "end of loop: " << i + 1 << " we get int: " << sent_int << std::endl;
         }
-        else{
-            MPI_Send(&sent_int, 1, MPI_INT, (rank + 1), 0, MPI_COMM_WORLD);
+        else {
+            //recieve from left neighbor
+            MPI_Recv(&sent_int, 1, MPI_INT, (rank - 1), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            std::cout << "Recieved int: " << sent_int << " on rank: " << rank << std::endl;
+            sent_int++;
+            
+            //if last process, send to 0, otherwise end to right neighbor
+            if(rank == size - 1){
+                //send to start process
+                std::cout << "sending back to process 0: " << sent_int << std::endl;
+                MPI_Send(&sent_int, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            }
+            else{
+                //send to right neighbor
+                MPI_Send(&sent_int, 1, MPI_INT, (rank + 1), 0, MPI_COMM_WORLD);
+            }
         }
     }
     
